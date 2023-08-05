22fc92cdc2120d59876411606cf45dcdf483a53b
ezfo <69818555+LeakedBuffalo7907@users.noreply.github.com>
2023-08-05 02:00:12 -0400

    sss

---
+++ client/Proximity.lua
@@ -0,0 +1,52 @@
+if periphemu then -- probably on CraftOS-PC
+    periphemu.create("back","speaker")
+    config.set("standardsMode",true)
+end
+
+local args = {...}
+local Proximity = {}
+local speaker = peripheral.find("speaker")
+local ConnectedWebsocket
+local buffer = {}
+
+function Proximity.run() 
+        local server = "ws://localhost:8080"
+        local ok, err = http.checkURL(server:gsub("^ws://", "http://"):gsub("^wss://", "https://"))
+
+        if ok then
+            local websocket = http.websocket(server)
+
+            if websocket ~= false then
+                term.clear()
+                term.write("Connected To : ")
+                term.setTextColor(colors.blue)
+                print(server)
+                term.setTextColor(colors.white)
+                ConnectedWebsocket = websocket
+                while true do
+                    event, url, message = os.pullEvent("websocket_message")
+                    if message ~= nil then
+                        term.setCursorPos(1,1)
+                        print(message)
+                        buffer = {("b"):rep(#message):unpack(message)}
+                        while not speaker.playAudio(buffer) do
+                            os.pullEvent("speaker_audio_empty")
+                        end
+                    end
+
+                end
+            end
+        else
+        term.setCursorPos(1,1)
+        print(err)
+        end
+    
+end
+
+
+
+
+command = table.remove(args, 1)
+if Proximity[command] then
+    Proximity[command](args)
+end
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 }); // Use any port you like
+console.log("Proximity WS Started")
+const connectedClients = new Set();
+
+wss.on('connection', (ws) => {
+  console.log('Client connected.');
+  connectedClients.add(ws);
+
+  ws.on('message', (message) => {
+      //console.log(message)
+      broadcast(message);
+  });
+
+  ws.on('close', () => {
+    console.log('Client disconnected.');
+    connectedClients.delete(ws);
+  });
+});
+
+function broadcast(message) {
+    const messageData = JSON.stringify(message);
+    for (const client of connectedClients) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(messageData);
+      }
+    }
+  }
+
+
